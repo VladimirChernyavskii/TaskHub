cbc3b39dcb75d9ca8d9c4d8990df773eeb32925c
Vladimir Chernyavskii <vladimir151055@gmail.com>
2025-10-31 17:29:26 +0600

    feat(core): initial project setup with webpack, ts, react

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	semi: true,
+	trailingComma: 'es5',
+	singleQuote: true,
+	jsxSingleQuote: true,
+	bracketSameLine: true, 
+	printWidth: 80,
+	useTabs: false,
+	tabWidth: 2,
+	endOfLine: 'lf',
+};
